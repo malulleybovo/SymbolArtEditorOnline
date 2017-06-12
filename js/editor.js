2abe7a13aec729e3673ca8b5aa93eb147e7d0c6e
malulleybovo <malulleybovo@wisc.edu>
2017-06-12 12:24:46 -0500

    Bug Testing Code Cleanup

--- js/editor.js
+++ js/editor.js
@@ -87,18 +87,10 @@ var Editor = Class({
         }).hide();
         function changeVertices(that, ix, iy, clientPos) {
             var canvasPos = $('canvas').offset();
-            alert('that: ' + that);
-            alert('that.list: ' + that.list);
-            alert('that.list.selectedElem: ' + that.list.selectedElem);
-            alert('that.list.selectedElem.parentNode: ' + that.list.selectedElem.parentNode);
-            alert('that.list.selectedElem.parentNode.elem: ' + that.list.selectedElem.parentNode.elem);
             var pos = { left: clientPos.left - canvasPos.left, top: clientPos.top - canvasPos.top };
             var layer = that.list.selectedElem.parentNode.elem;
-            alert('layer: ' + layer + '; ' + layer.x + ', ' + layer.y);
-            alert(pos.left + ', ' + pos.top);
             layer.vertices[ix] = Math.round((pos.left / that.list.editor.zoom) - layer.x);
             layer.vertices[iy] = Math.round((pos.top / that.list.editor.zoom) - layer.y);
-            alert(layer.vertices[ix] + ', ' + layer.vertices[iy]);
             that.list.editor.updateLayer(layer);
             that.list.editor.render();
         }
@@ -125,11 +117,11 @@ var Editor = Class({
                     left: Math.round(e.clientX),
                     top: Math.round(e.clientY)
                 }
+                $(buttons[0]).css({ top: (pos.top - 22.8), left: (pos.left - 14.8) });
                 changeVertices(buttons[0], 0, 1, pos);
                 var layerCtrl = $('#' + layerCtrlID)[0].layerCtrl;
                 layerCtrl.v0.updateDisplay();
                 layerCtrl.v1.updateDisplay();
-                $(buttons[0]).css({ top: (pos.top - 22.8), left: (pos.left - 14.8) });
             }
             else if (buttons[1].moving) {
                 var pos = {
